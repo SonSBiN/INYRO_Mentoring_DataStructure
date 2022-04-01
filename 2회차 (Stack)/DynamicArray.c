f8b339e3431ea5b387cf0042969f31d0ebde88ff
SonSBiN <victor990914@naver.com>
2022-04-01 12:40:26 +0900

    INYRO

--- 2회차 (Stack)/DynamicArray.c
+++ 2회차 (Stack)/DynamicArray.c
@@ -7,16 +7,19 @@
 
 DynamicArray* DACreate()
 {
-    DynamicArray* resizableArray = (DynamicArray*) malloc(sizeof(DynamicArray));
-    if (resizableArray) {
-        resizableArray->arr = (int*)malloc(INCREMENT_SIZE * sizeof(Element));
-        if (resizableArray->arr) {
-            resizableArray->size = INCREMENT_SIZE;
-            resizableArray->count = 0;
-            return resizableArray;
-        }
-    }
-    return NULL;
+	DynamicArray* da = (DynamicArray*)malloc(sizeof(DynamicArray));
+	if (da) {
+		da->arr = (Element*)malloc(sizeof(Element) * INCREMENT_SIZE);
+		if (da->arr) {
+			da->size = INCREMENT_SIZE;
+			da->count = 0;
+			return da;
+		}
+		else {
+			free(da);
+		}
+	}
+	return NULL;
 }
 
 int DAGetCount(const DynamicArray* resizableArray)
@@ -37,48 +40,50 @@ int DAGetSize(const DynamicArray* resizableArray)
 
 int DAAdd(DynamicArray* resizableArray, Element value)
 {
-    if (resizableArray) {
-        if (resizableArray->count == resizableArray->size) {
-            resizableArray->size += INCREMENT_SIZE;
-            resizableArray->arr = realloc(resizableArray->arr, resizableArray->size * sizeof(int));
-            if (resizableArray->arr == NULL) return 0;
-        }
-        resizableArray->arr[resizableArray->count] = value;
-        resizableArray->count++;
-        return 1;
-    }
-    return 0;
+	if (resizableArray && resizableArray->arr) {
+		if (resizableArray->count > 0 && resizableArray->count % INCREMENT_SIZE == 0) {
+			resizableArray->size += INCREMENT_SIZE;
+			resizableArray->arr = (Element*)realloc(resizableArray->arr, sizeof(Element) * resizableArray->size);
+			if (resizableArray->arr == NULL) {
+				free(resizableArray);
+				return 0;
+			}
+		}
+		resizableArray->arr[resizableArray->count] = value;
+		resizableArray->count++;
+		return 1;
+	}
+	return 0;
 }
 
 int DAPut(DynamicArray* resizableArray, int idx, Element value)
 {
-    if (resizableArray && idx < resizableArray->size) {
-        resizableArray->arr[idx] = value;
-        return 1;
-    }
-    return 0;
+	if (resizableArray && resizableArray->arr 
+		&& idx >= 0 && idx < resizableArray->size) {
+		resizableArray->arr[idx] = value;
+		return 1;
+	}
+	return 0;
 }
 
 int DAGet(const DynamicArray* resizableArray, int idx, Element* value)
 {
-    if (resizableArray && idx < resizableArray->count) {
-        *value = resizableArray->arr[idx];
-        return 1;
-    }
-    return 0;
+	if (resizableArray && resizableArray->arr
+		&& idx >= 0 && idx < resizableArray->size) {
+		*value = resizableArray->arr[idx];
+		return 1;
+	}
+	return 0;
 }
 
 int DADestroy(DynamicArray* resizableArray)
 {
-    if (resizableArray && resizableArray->arr) {
-        free(resizableArray->arr);
-        resizableArray->arr = NULL;
-        resizableArray->size = 0;
-        resizableArray->count = 0;
-        free(resizableArray);
-        return 1;
-    }
-    return 0;
+	if (resizableArray && resizableArray->arr) {
+		free(resizableArray->arr);
+		free(resizableArray);
+		return 1;
+	}
+	return 0;
 }
 
 int DAPrint(const DynamicArray* resizableArray, PrintFunc printFunc)
